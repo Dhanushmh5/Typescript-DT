e6747369efe99af82dc03764834e4ad8d175ca52
Maxi <dhanushmh5@gmail.com>
2025-12-01 10:30:34 +0000

    Number and Boolean in TS

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -1,2 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = 'Dhanush';
 console.log(greetings);
+// greetings=true //error due to type mismatch
+// greetings.toLowercase()
+//number
+var userid = 2233445;
+var str = userid.toLocaleString();
+//boolean
+var isLoggedIn = false;
+console.log(isLoggedIn);
+//Type Inference
+var num = 234.45; //--> TS is smart enough to infer type in such cases avoid colon
+num.toFixed();
--- 01_basics/variable.ts
+++ 01_basics/variable.ts
@@ -5,4 +5,21 @@ console.log(greetings)
 
 // greetings.toLowercase()
 
+//number
+
+let userid:number=2233445
+let str:string=userid.toLocaleString()
+
+//boolean
+
+let isLoggedIn:boolean=false
+console.log(isLoggedIn)
+
+//Type Inference
+
+let num=234.45//--> TS is smart enough to infer type in such cases avoid colon
+num.toFixed()
+// num='asjc'//error
+
+
 export {} //temporary way to avoid error
