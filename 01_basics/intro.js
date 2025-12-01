b7996ff7bd9b66d0215771783aefc993600e5038
Maxi <dhanushmh5@gmail.com>
2025-12-01 09:41:00 +0000

    Installation and TSC

---
+++ 01_basics/intro.js
@@ -0,0 +1,3 @@
+console.log('Dhanush'); //Classic Javascript
+var user = { name: 'Abc', age: 21 };
+console.log(user.email); //errrs raised only after execution of code
---
+++ 01_basics/intro.ts
@@ -0,0 +1,6 @@
+console.log('Dhanush')//Classic Javascript
+
+const user = {name:'Abc',age:21}
+
+console.log(user.email)// gives an error since there is no such member in user
+//speciality of typescript
