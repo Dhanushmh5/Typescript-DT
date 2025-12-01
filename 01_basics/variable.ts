328ff2c3df7e1311ac0e9df03b3fb547779e1d7a
Maxi <dhanushmh5@gmail.com>
2025-12-01 10:37:30 +0000

    Any and noImplicitAny

--- 01_basics/variable.ts
+++ 01_basics/variable.ts
@@ -21,5 +21,17 @@ let num=234.45//--> TS is smart enough to infer type in such cases avoid colon
 num.toFixed()
 // num='asjc'//error
 
+// any
+
+let hero; //any datatype
+
+function getHero(){
+    return "thor"
+}
+
+hero = getHero()
+
+
+
 
 export {} //temporary way to avoid error
