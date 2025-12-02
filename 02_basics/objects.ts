e0ac39a00b5eb6b5125b279efdcc9519b01734d0
Maxi <dhanushmh5@gmail.com>
2025-12-02 10:45:51 +0000

    Object Behaviour in TS

---
+++ 02_basics/objects.ts
@@ -0,0 +1,43 @@
+const User = {
+    name: "hitesh",
+    email: "hitesh@lco.dev",
+    isAvtive: true
+}
+
+function createUser({name: string, isPaid: boolean}){}
+
+let newUser = {name: "hitesh", isPaid: false, email: "h@h.com"}
+
+createUser(newUser)
+
+
+
+function createCourse():{name: string, price: number}{//argument is also an object
+    return {name: "reactjs", price: 399} //return type in object format
+}
+
+
+// type User = {
+//     name: string;
+//     email: string;
+//     isActive: boolean
+// }
+
+
+// function createUser(user: User): User{
+//     return {name: "", email: "", isActive: true}
+// }
+
+// createUser({name: "", email: "", isActive: true})
+
+
+
+
+
+
+
+
+
+
+
+export {}
