8b1ba1a2b0b711a3ecf675751ffad5e1bcd9f284
Maxi <dhanushmh5@gmail.com>
2025-12-02 10:33:09 +0000

    Functions-2

--- 02_basics/function.ts
+++ 02_basics/function.ts
@@ -21,4 +21,36 @@ function signUpUser(name: string, email: string, isPaid: boolean){}
 signUpUser("hitesh", "hitesh@lco.dev", false)
 
 
+// function getVal(myVal:number){
+//     if(myVal>5){
+//         return true;
+
+//     }
+//     else{
+//         return '200 ok'
+//     }
+// }
+//accepatble code how to handle more than one return data type
+
+const heros = ["thor", "spiderman", "ironman"]
+// const heros = [1, 2, 3]
+
+heros.map((hero): string => {
+    return `hero is ${hero}`//automatically intitaes into string for hero
+    //context switching of TS is clean
+})
+
+function consoleError(errmsg: string): void{
+    console.log(errmsg);
+    
+}
+
+function handleError(errmsg: string): never{
+    throw new Error(errmsg);//never is advisable to use for handling errors 
+    //function stops execution or
+    
+}
+
+
+
 export {}
