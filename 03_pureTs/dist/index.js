7f66ebfee2cd93385b136cb6fbcc57c0ac54cbd0
Maxi <dhanushmh5@gmail.com>
2025-12-03 12:32:15 +0000

    Production level settings for using TS

---
+++ 03_pureTs/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('------Hello Typescript -----');
+console.log('Hey JS');
+//# sourceMappingURL=index.js.map
