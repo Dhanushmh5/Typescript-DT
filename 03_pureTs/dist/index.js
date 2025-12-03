51b425ba326c1aa6fc31851c28f8edc0d40931da
Maxi <dhanushmh5@gmail.com>
2025-12-03 12:43:09 +0000

    Classes in TS

--- 03_pureTs/dist/index.js
+++ 03_pureTs/dist/index.js
@@ -1,5 +1,16 @@
 "use strict";
+// console.log('------Hello Typescript -----')
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log('------Hello Typescript -----');
-console.log('Hey JS');
+// console.log('Hey JS')
+//Classes in TS
+class User {
+    name;
+    email;
+    city = "";
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+}
+const obj = new User('Dhanush', 'abc@gmail.com');
 //# sourceMappingURL=index.js.map
--- 03_pureTs/src/index.ts
+++ 03_pureTs/src/index.ts
@@ -1,3 +1,17 @@
-console.log('------Hello Typescript -----')
+// console.log('------Hello Typescript -----')
 
-console.log('Hey JS')
+// console.log('Hey JS')
+
+//Classes in TS
+
+class User{
+    name:string
+    email:string
+    city:string=""
+    constructor(name:string,email:string){
+        this.name=name
+        this.email=email
+    }
+}
+
+const obj=new User('Dhanush','abc@gmail.com')
