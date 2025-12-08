b5ad0cec69e9aca2c31564ef9cdfabc48e5bd413
Maxi <dhanushmh5@gmail.com>
2025-12-08 14:06:23 +0000

    Getter and Setters

--- 03_pureTs/src/index.ts
+++ 03_pureTs/src/index.ts
@@ -8,10 +8,24 @@ class User{
     name:string
     private email:string //access specifiers can only be used in TS
     public city:string=""
-    constructor(name:string,email:string){
+    constructor(name:string,email:string,public id:number){
         this.name=name
         this.email=email
+        this.id=id
+    }
+
+    get getID():string{
+        return `abc${this.email}` //getter in TS
+    }
+    set getID(id:number){ //setter for the declared getter setter has no return type so dont define it as void
+        if(id<3){
+            this.id=id
+        }
+        else{
+            throw new Error("there is an error in Id");
+            
+        }
     }
 }
 
-const obj=new User('Dhanush','abc@gmail.com')
+const obj=new User('Dhanush','abc@gmail.com',123)
